eb58be5424a732074a737975b3385d4ad5dd7e30
André Oliveira <afelip.co@gmail.com>
2022-08-16 09:20:06 -0300

    Aula 05

---
+++ aula_05.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    lista = ['André', 'Charles do Bronks', 'Ebony Bitch']
+    tupla = ('André', 'Charles do Bronks', 'Ebony Bitch')
+    print(lista)
+    print(tupla)
+    print(type(lista))
+    print(type(tupla))
